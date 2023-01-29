67a3b91d818731620a3deeb8aa7dc3f7a29e7ce0
Neil Roberts <neilr@runbox.com>
2023-01-29 12:53:39 -0800

    Implement an API to query the service

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 )
 
@@ -10,10 +8,6 @@ func main() {
 	svc := NewCatFactService("https://catfact.ninja/fact")
 	svc = NewLoggingService(svc)
 
-	fact, err := svc.GetCatFact(context.TODO())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("%+v\n", fact)
+	apiServer := NewApiServer(svc)
+	log.Fatal(apiServer.Start(":3000"))
 }
